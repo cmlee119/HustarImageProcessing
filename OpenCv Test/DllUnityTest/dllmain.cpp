bb9c556d7a70189acb970b6a511090d760fb26ba
cmlee119 <60453679+cmlee119@users.noreply.github.com>
2020-07-08 17:25:41 +0900

    Unity에서 구동 가능한 DllUnityTest 프로젝트 커밋

--- OpenCv Test/DllUnityTest/dllmain.cpp
+++ OpenCv Test/DllUnityTest/dllmain.cpp
@@ -10,18 +10,28 @@ const int ESC = 27;
 
 extern "C"
 {
-    __declspec(dllexport) unsigned char* processFrame(int& witdh, int& height) {
-
+    __declspec(dllexport) void GetRawImageBytes(unsigned char* data, int width, int height)
+    {
         VideoCapture inputVideo = VideoCapture("C:/Users/cmlee/source/repos/OpenCv Test/test.mp4");
 
         Mat input_image;
         inputVideo.read(input_image);
 
-        cv::Size s = input_image.size();
-        witdh = s.height;
-        height = s.width;
+        //Resize Mat to match the array passed to it from C#
+        cv::Mat resizedMat(height, width, input_image.type());
+        cv::resize(input_image, resizedMat, resizedMat.size(), cv::INTER_CUBIC);
 
-        imshow("test", input_image);
-        return input_image.data;
+        //You may not need this line. Depends on what you are doing
+        cv::imshow("Nicolas", resizedMat);
+
+        //Convert from RGB to ARGB 
+        cv::Mat argb_img;
+        cv::cvtColor(resizedMat, argb_img, cv::COLOR_RGB2BGRA);
+        std::vector<cv::Mat> bgra;
+        cv::split(argb_img, bgra);
+        std::swap(bgra[0], bgra[3]);
+        std::swap(bgra[1], bgra[2]);
+        std::memcpy(data, argb_img.data, argb_img.total() * argb_img.elemSize());
     }
 }
+
